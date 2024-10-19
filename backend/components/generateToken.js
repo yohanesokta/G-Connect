5fa63b04db60b37e5fad4a818e94079cc0a173fa
yohanesokta <yohanesoktayt72@gmail.com>
2024-10-20 02:49:46 +0700

    change : feature backend & readme

--- backend/components/generateToken.js
+++
@@ -1,6 +0,0 @@
-const generateToken = () => {
-    genNum = () => Math.floor(Math.random(1) * 10)
-    return `${genNum()} ${genNum()} ${genNum()} ${genNum()} ${genNum()} ${genNum()}`
-}
-
-module.exports = generateToken
